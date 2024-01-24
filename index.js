46b0e1ab29b342080d6f1ba94c5a5d0892346331
Mayank Negi <mayankn792@gmail.com>
2024-01-25 01:50:09 +0530

    Add videos search endpoint

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+require('dotenv').config()
+
 const express = require('express')
+const {google} = require('googleapis')
+
 const app = express()
 
-require('dotenv').config()
+const apiKey = process.env.YT_API_KEY
+const yt = google.youtube({version: 'v3', auth: apiKey})
 
 app.get('/', (req, res) => {
     res.send('UP')
+})
+
+const getSearch = async (query) => {
+    const res = await yt.search.list({
+        part: 'snippet',
+        q: query
+    })
+    return res.data.items
+}
+
+app.get('/search/:query', async (req, res) => {
+    const query = req.params.query
+    const resp = await getSearch(query)
+    res.send(resp)
 })
 
 app.listen(process.env.PORT, ()=> {
